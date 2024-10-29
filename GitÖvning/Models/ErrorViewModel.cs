2f872a2fc933f5bfaea1fd940374cddd7c9e1d0c
Ammara Shakeel <ammarashakeel81@yahoo.se>
2024-10-29 14:32:16 +0100

    Add project files.

---
+++ GitÖvning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GitÖvning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
